c34d988058a8a6a7e29aa5bf8703cb2978f6c29c
ttstase <ttstase@gmail.com>
2023-06-02 00:17:24 +0200

    5commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class MainTest {
 
     @Test(dataProvider = "persons", dataProviderClass = TestDataProvider.class)
-    public void testGetFirstName(String firstName, String lastName, int age) {
+    public void testGetPersonsData(String firstName, String lastName, int age) {
         Persons person = new Persons();
         System.out.println(firstName + " " + lastName + " " + age);
         Assert.assertEquals(person, person);
