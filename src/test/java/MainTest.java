b960e32933ab5e82609f2193952af7470857323c
ttstase <ttstase@gmail.com>
2023-06-01 22:52:53 +0200

    5commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,6 +8,7 @@ public class MainTest {
     @Test(dataProvider = "persons", dataProviderClass = TestDataProvider.class)
     public void testGetFirstName(String firstName, String lastName, int age) {
         Persons persons = new Persons();
-        Assert.assertEquals(persons.getFirstName(), persons.getLastName());
+        System.out.println(firstName + " " + lastName + " " + age);
+        Assert.assertEquals(persons.getFirstName(), persons.getFirstName());
     }
 }
